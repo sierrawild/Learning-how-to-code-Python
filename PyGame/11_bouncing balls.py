bb5e5559ee31f761042e9a8755aede622dc0bb1f
Pawel <pawelbabiak@gmail.com>
2025-12-31 01:02:27 +0000

    Going with a bang

--- PyGame/11_bouncing balls.py
+++ PyGame/11_bouncing balls.py
@@ -5,6 +5,15 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 1080))
 clock = pygame.time.Clock()
 
+speed = [2,2]
+
+pos1 = pygame.Vector2(100,100)
+radius = 50
+
+ball_1rec = pygame.Rect(0,0, radius * 2, radius * 2)
+
+
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -12,9 +21,14 @@ while running:
         or pygame.key.get_pressed()[pygame.K_ESCAPE]:
             running = False
             
-    screen.fill("#7AA0AD")
-    pygame.display.flip()
     
+            
+    screen.fill("#7AA0AD")
+    
+    ball_1rec.center = pos1
+    ball_1 = pygame.draw.circle(screen, "red", ball_1rec.center, radius)
+    
+    pygame.display.flip()
     
     clock.tick(60)
     
