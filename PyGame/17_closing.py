47d5e7c161e4f9f41c6992392cf54bf6927cdefb
Pawel <pawelbabiak@gmail.com>
2026-01-11 23:07:16 +0000

    feat: add initial pygame window with quit handling and dynamic resizing

---
+++ PyGame/17_closing.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
+
+clock = pygame.time.Clock()
+
+
+tryingToQuit = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            if tryingToQuit < 3:
+                width += 20
+                height += 20
+                tryingToQuit += 1
+                screen = pygame.display.set_mode((width, height))
+            elif tryingToQuit < 5:
+                tryingToQuit += 1
+                running = False
+            else:
+                running = False
+                
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+    
+    screen.fill("#FFDBDB")
+    
+    
+    
+    pygame.display.flip()
+    
+    clock.tick(60)
+
+if tryingToQuit < 6:
+    screen2 = pygame.display.set_mode((width/2, height)) 
+    # pygame.quit()
+else:
+    pygame.quit()
