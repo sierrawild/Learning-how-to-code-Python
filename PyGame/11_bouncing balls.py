9adef8ff1184c81405bd43e76a1efd773b123098
Pawel <pawelbabiak@gmail.com>
2026-01-14 19:33:22 +0000

    feat: update screen dimensions to use defined constants for better maintainability

--- PyGame/11_bouncing balls.py
+++ PyGame/11_bouncing balls.py
@@ -1,8 +1,10 @@
 import pygame
 
 pygame.init()
+WIDTH = 500
+HEIGHT = 500
 
-screen = pygame.display.set_mode((1280, 1080))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 speed = 10
