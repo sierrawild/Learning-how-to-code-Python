83618cc86e1d8ee88ef4b9bbd46b2eace9174814
Pawel <pawelbabiak@gmail.com>
2026-02-06 19:41:46 +0000

    feat: add grid rendering with dynamic color changes in Pygame

---
+++ PyGame/23 grid.py
@@ -0,0 +1,48 @@
+import pygame
+
+pygame.init()
+
+width = 1000
+height = 1000
+
+spacing = 60
+rec_size = 40
+
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+time = 0
+square_color = ["#FFFFFF","#97FFD0", "#A397FF", "#FFFC97", "#EE97FF", "#FFA797"]
+
+
+def draw_sqr(spacing, rec_size, screen, color, j, i):
+    pygame.draw.rect(screen, color, (spacing * i, spacing * j, rec_size, rec_size))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill("black")
+    color = square_color[int(time/60)]
+    for j in range(int(height/ spacing) +1):
+        for i in range(int(width/spacing) +1):
+            if i % 2 == 0 and j % 2 == 0:
+                draw_sqr(spacing, rec_size, screen, color, j, i)
+            if i % 2 != 0 and j % 2 != 0:
+                draw_sqr(spacing, rec_size, screen, color, j, i)
+                
+        
+    pygame.display.flip()
+
+    time += 1
+    if time == 60 * 6:
+        time = 0
+        
+    # print(f"{color=}\n{int(time/60)} \n\n {time=}")
+        
+    clock.tick(60)
+    
+
+pygame.quit()
