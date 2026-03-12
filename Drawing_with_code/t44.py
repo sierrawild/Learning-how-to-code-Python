3348b7e2f77ceebd007b8b7bb0a8c2c8692243c8
Pawel <pawelbabiak@gmail.com>
2026-03-12 20:56:14 +0000

    drawing with python

--- Drawing_with_code/t44.py
+++ Drawing_with_code/t44.py
@@ -3,12 +3,30 @@ import random
 
 hideturtle()
 speed(0)
+random.seed(1)
 
-for i in range(10):
+xx, yy = 150, 250
+
+for i in range(1000):
     forward(random.randint(2,100))
     right(90)
     
     x,y = pos()
+    
+    if x > xx:
+        setheading(180)
+        forward(random.randint(50,100))
+    elif x < -xx:
+        setheading(0)
+        forward(random.randint(50,100))
+    elif y > yy:
+        setheading(270)
+        forward(random.randint(50,100))
+    elif y < -yy:
+        setheading(90)
+        forward(random.randint(50,100))
+        
+    
     
 print(f"{x=} {y=}")
 
