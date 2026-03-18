e4377f7d04f5dbf5180487777422f248be7e7e79
Pawel <pawelbabiak@gmail.com>
2026-03-18 22:21:55 +0000

    chaos game in pygame

---
+++ PyGame/Chaos_Game.py
@@ -0,0 +1,11 @@
+import pygame, random
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+
