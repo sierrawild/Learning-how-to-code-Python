20c9b3e362d1d060e1a7ce7aab7bc4daf71e2b72
Pawel <pawelbabiak@gmail.com>
2026-03-31 19:14:38 +0100

    pygame template cleanup

--- PyGame/0_how to start project.py
+++ PyGame/0_how to start project.py
@@ -1,4 +1,3 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 
 # pygame setup
@@ -8,7 +7,6 @@ clock = pygame.time.Clock()
 
 running = True
 while running:
-    # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
