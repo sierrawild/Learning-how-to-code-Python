0ecb0e54461c37f984f84abc86c01afa3cf82943
Pawel <pawelbabiak@gmail.com>
2026-04-10 21:33:13 +0100

    troubleshooting py5

--- py5/01_day.py
+++ py5/01_day.py
@@ -1,7 +1,7 @@
 from py5 import *
 
 def setup():
-    size(500, 500)
+    size(100, 500)
     fill("#CFFFE9")
     rect(100, 100, 200, 300)
 
--- py5/03.py
+++ py5/03.py
@@ -1,11 +1,11 @@
 from py5 import *
 
-def setting():
+def settings():
     size(1000, 1000, P2D)
     
     
 def setup():
-    background(240)  # Light gray background so we can see both shapes
+    background("#98F7CC")  # Light gray background so we can see both shapes
 
 def draw():
     no_loop()  # Only draw once
