172957a8ee4d4e88d83d8bba1ae03d9dcd7bbd63
Pawel <pawelbabiak@gmail.com>
2026-04-14 21:54:05 +0100

    Infection grid with reverse mechanics

--- py5/06.grid 2 random infecting recursive.py
+++ py5/06.grid 2 random infecting recursive.py
@@ -33,49 +33,58 @@ def draw():
     py5.translate(50,50)
     py5.background(monochrome_palette[0])
     
+    if grid_not_full:
+        draw_grid()
+        
+        # wait for time to infect and refine next square to infect        
+        if py5.frame_count % (time_to_infect) == 0:
+            random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
+            
+            # check if infected square have been already infected. 
+            while random_square in infected_cells and grid_not_full:
+                # If square in infected_cell chose another square
+                random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
+                # if no of infected squares >= to the whole grid, brake the while loop
+                if len(infected_cells) >= (grid_size*grid_size):
+                    print("Grid is full")
+                    grid_not_full = False
+            # after all above checks are satisfied, append the list of squares
+            infected_cells.append(random_square)       
+        
+        # draw infected cells
+        for i in infected_cells:
+            py5.fill(monochrome_palette[3])
+            py5.rect(i[0]*spacing,i[1]*spacing, size+10, 10+size)
+     
+    # reverse infection (heal the cells)   
+    if grid_not_full == False:
+        py5.background(monochrome_palette[0])
+        py5.fill(monochrome_palette[1])
+        draw_grid()
+        for i in infected_cells:
+            py5.fill(monochrome_palette[3])
+            py5.rect(i[0]*spacing,i[1]*spacing, size+10, 10+size)
+            
+        if py5.frame_count % (time_to_infect) == 0:
+            infected_cells.pop(-1)
+            # check if grid is empty
+            if len(infected_cells) == 0:
+                print("Grid is empty")
+                grid_not_full = True
+
+def draw_grid():
     for j in range(grid_size):
         for i in range(grid_size):
             py5.fill(monochrome_palette[1])
             py5.rect(j*spacing,i*spacing, size, size, 20)
-            
-    recursive_infection(0)      
-    
-    # if py5.frame_count % (60 * 0.1) == 0:
-    #     random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
         
-    #     while random_square in infected_cells and grid_not_full:
-    #         random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
-    #         if len(infected_cells) >= (grid_size*grid_size):
-    #             print("Grid is full")
-    #             grid_not_full = False
-    #     infected_cells.append(random_square)       
-    
-    # for i in infected_cells:
-    #     py5.fill(monochrome_palette[3])
-    #     py5.rect(i[0]*spacing,i[1]*spacing, size+10, 10+size)
-            
+        
 grid_size = 6   
 size = 80
 infected_cells = []
-grid_not_full = True   
+grid_not_full = True 
+time_to_infect = 10 # 6 = 1s
 
-def recursive_infection(n):
-    global infected_cells
-    if n > 36:
-        print("Grid is full")
-        return
-    else:
-        # if py5.frame_count % (60 * 0.1) == 0:
-            
-        random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
-        while random_square in infected_cells:
-            random_square = [py5.random_int(0,grid_size-1), py5.random_int(0,grid_size-1)]
-        infected_cells.append(random_square)
-        for i in infected_cells:
-            py5.fill(monochrome_palette[3])
-            py5.rect(i[0]*spacing,i[1]*spacing, size+10, 10+size)
-        
-        recursive_infection(n+1)
-        
+       
 
 py5.run_sketch()
