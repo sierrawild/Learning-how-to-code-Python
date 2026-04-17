f98c93ad6a070e0bbfb3bc75f036ce52714e91b0
Pawel <pawelbabiak@gmail.com>
2026-04-17 22:03:45 +0100

    clearing sketch with right button

--- py5/sctratch_art.py
+++ py5/sctratch_art.py
@@ -1,7 +1,7 @@
 import py5
 
 def settings():
-    py5.size(800,400)
+    py5.size(1500,1000)
     
 def setup():
     py5.frame_rate(120)
@@ -19,5 +19,9 @@ def draw():
     if py5.is_mouse_pressed and py5.mouse_button == py5.LEFT:
         py5.line(py5.mouse_x, py5.mouse_y, py5.pmouse_x, py5.pmouse_y)
     # py5.circle(py5.mouse_x, py5.mouse_y, 15)
+
+    # reset sketch
+    if py5.is_mouse_pressed and py5.mouse_button == py5.RIGHT:
+        py5.background('#000000')
     
 py5.run_sketch()
