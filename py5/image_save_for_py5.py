822eb07c7059cc11f7c64f960443a06a2c65befd
Pawel <pawelbabiak@gmail.com>
2026-05-05 18:58:05 +0100

    implementing limit in save function

--- py5/image_save_for_py5.py
+++ py5/image_save_for_py5.py
@@ -1,7 +1,7 @@
 import sys, os, py5
 
 
-def save_img(folder_name='0'):
+def save_img(folder_name='0', top_limit=1000, bottom_limit=0):
     # Get the sketch filename without extension
     sketch_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
     base_path = f"D:/VIDEO/py5/{sketch_name}/{folder_name}/"
@@ -15,4 +15,5 @@ def save_img(folder_name='0'):
         print(f'Files will save to: {base_path}frame_{no_frame}.png')
     
     # save the image
-    py5.save_frame(f'{base_path}frame_{no_frame}.png')   
+    if py5.frame_count <= top_limit and py5.frame_count >= bottom_limit:
+        py5.save_frame(f'{base_path}frame_{no_frame}.png')   
