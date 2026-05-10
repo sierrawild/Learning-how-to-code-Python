105418e44f62a8c148a96023a205d6394506f4cf
Pawel <pawelbabiak@gmail.com>
2026-05-10 22:12:46 +0100

    refactor: rename variable 'p' to 'pallet' for clarity and consistency

--- py5/08 Rotating things - Piter Pasmas copy.py
+++ py5/08 Rotating things - Piter Pasmas copy.py
@@ -3,7 +3,7 @@
 import py5, palette
 
 W, H = 1000, 1000
-p = palette.INK
+pallet = palette.INK
 
 def settings():
     py5.size(W,H)
@@ -12,18 +12,38 @@ def settings():
 def setup():
     py5.frame_rate((60))
     
-    py5.background(*p['bg'])
-    py5.stroke(*p['colors'][0])
-    py5.fill(*p['colors'][1])
+    py5.background(*pallet['bg'])
+    py5.stroke(*pallet['colors'][0])
+    py5.fill(*pallet['colors'][1])
     py5.stroke_weight(5)
     
     py5.no_fill()
     # py5.no_stroke()
     
 def draw():
-    py5.background(*p['bg'])
+    py5.background(*pallet['bg'])
+    
+    py5.push_matrix()
+    py5.translate(W/2,H/2)
+    for i in range(100):
+        f = py5.frame_count / 60
+        p = i / 100
+        
+        pos = rotating_thing(f,p, 100, 1)
+        
+        py5.circle(pos['x'], pos['y'], 5) 
     
     
+    py5.pop_matrix()
+
+def rotating_circle(phi, r):
+    return vec2(r * py5.cos(phi), r * py5.sin(phi))
+
+
+def rotating_thing(f,p,a,s):
+    return rotating_circle((s * f + p) * py5.TAU, a)
+
+  
 def vec2(x,y):
     return{"x":x, "y":y}
 
