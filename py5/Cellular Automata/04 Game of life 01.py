75e10a2a8143dddb2c782db43459c64803a68b35
Pawel <pawelbabiak@gmail.com>
2026-05-17 22:12:46 +0100

    Refactor frame rate and add frame count display

--- py5/Cellular Automata/04 Game of life 01.py
+++ py5/Cellular Automata/04 Game of life 01.py
@@ -28,13 +28,14 @@ grid[5][7] = 1
 
 
 
+speed = 10
+is_paused = False
 
 def settings():
     py5.size(W,H)
-    # py5.full_screen()
     
 def setup():
-    py5.frame_rate(10)
+    py5.frame_rate(speed)
     
     py5.background(*p['bg'])
     py5.stroke(*p['bg'])
@@ -57,6 +58,15 @@ def draw():
             py5.square(x *cell_size,y * cell_size, cell_size)
 
         
+    display_frame_count()
+
+def display_frame_count():
+    py5.push_style()
+    py5.fill("#FFFFFF")
+    py5.text_align(py5.CENTER)
+    py5.text_size(18)
+    py5.text(py5.frame_count, 20, 20)
+    py5.pop_style()
     
 def mouse_pressed():
     x = py5.mouse_x // cell_size
@@ -64,7 +74,10 @@ def mouse_pressed():
     
     grid[x][y] = 1 - grid[x][y]
     
-
-    
+def key_pressed():
+    global is_paused
+    if py5.key == " ":
+        is_paused = not is_paused
+        # to get this work I will have to change how update grid work and display different counter 
 
 py5.run_sketch()
