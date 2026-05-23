bca1cc6c37dbd94f3c2180f6002bff116e2e9d61
Pawel <pawelbabiak@gmail.com>
2026-05-23 08:17:16 +0100

    Failed attempt on 3rd pendulum

--- py5/Pendulum/04 Pendulum cycle copy.py
+++ py5/Pendulum/04 Pendulum cycle copy.py
@@ -29,11 +29,14 @@ def draw():
     angle = 0.08 * py5.frame_count % 360
     r = 100
     
+    
+    # first pendulum
     py5.push_matrix()
+    
     xy1 = 100
-    ham = 0.15
-    x1 = py5.cos(angle * ham) * xy1
-    y1 = py5.sin(angle * ham) * xy1
+    speed = 0.15
+    x1 = py5.cos(angle * speed) * xy1
+    y1 = py5.sin(angle * speed) * xy1
     py5.translate(x1, y1)
     x = py5.cos(angle) * r
     y = py5.sin(angle) * r
@@ -47,6 +50,27 @@ def draw():
     py5.circle(x,y,10)
     
     py5.pop_matrix()
+    
+    
+    # second pendulum
+    py5.push_matrix()
+    xy2 = 80
+    speed2 = 0.1
+    x2 = py5.cos(angle * speed2) * xy2
+    y2 = py5.sin(angle * speed2) * xy2
+    py5.translate(x2,y2)
+    
+    
+    # Pendulum 2
+    py5.stroke(*p['colors'][3])
+    py5.stroke_weight(5)
+    py5.line(0,0,x2,y2)
+    py5.circle(0,0,10)
+    # End of pendulum ball 2
+    py5.stroke(*p['colors'][3])
+    py5.circle(x2,y2,10)
+    py5.pop_matrix()
+    
     
     # inside circle
     py5.stroke(*p['colors'][0])
