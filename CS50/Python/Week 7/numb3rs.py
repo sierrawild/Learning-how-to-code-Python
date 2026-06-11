e47b3c0036f5ba257bad83c5721a37cc297e8db2
Pawel <pawelbabiak@gmail.com>
2026-06-11 22:39:36 +0100

    re

---
+++ CS50/Python/Week 7/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+ip = 
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
--- CS50/Python/Week 7/test.py
+++
@@ -1,6 +0,0 @@
-email = "hello@world.com"
-
-if "@" in email and "." in email:
-    print("💚")
-else:
-    print("❤")
