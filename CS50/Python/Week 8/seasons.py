f5e788139f970669b1fb26e973645dee0076a275
Pawel <pawelbabiak@gmail.com>
2026-06-17 16:55:32 +0100

    Refactor date of birth input and calculation of age in minutes

--- CS50/Python/Week 8/seasons.py
+++ CS50/Python/Week 8/seasons.py
@@ -1,10 +1,15 @@
 from datetime import date
 
 def main():
-    dob = input("When wore you born (format: YYY-MM-DD): ")
+    # dob = input("When wore you born (format: YYYY-MM-DD): ").strip().split("-")
+    dob = "1988-01-15".strip().split("-")
+    date_of_birth = date(int(dob[0]),int(dob[1]),int(dob[2]))
     today = date.today()
+    days = (today - date_of_birth).days
+    minutes = days * 24 * 60
     
-    print(f'{dob=} and {today=}')
+    print(minutes / 24 / 60 / 365)
     
 if __name__ == "__main__":
     main()
+    
