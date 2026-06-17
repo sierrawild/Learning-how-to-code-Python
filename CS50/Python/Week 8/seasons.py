f580a8a2de78470368f51596ec86ca2e0f95d92d
Pawel <pawelbabiak@gmail.com>
2026-06-17 22:04:24 +0100

    working on a problem

--- CS50/Python/Week 8/seasons.py
+++ CS50/Python/Week 8/seasons.py
@@ -1,15 +1,31 @@
 from datetime import date
+import inflect
+p = inflect.engine()
+today = date.today()
+today = "2003-01-01"
 
 def main():
-    # dob = input("When wore you born (format: YYYY-MM-DD): ").strip().split("-")
-    dob = "1988-01-15".strip().split("-")
-    date_of_birth = date(int(dob[0]),int(dob[1]),int(dob[2]))
-    today = date.today()
+    # dob = input("When wore you born (format: YYYY-MM-DD): ")
+    dob = "2001-01-01"
+    print(process(dob, today))
+
+def process(x, today):
+    if type(today) == str:
+        today = today.strip().split("-")
+        today = date(year=int(today[0]),month=int(today[1]),day=int(today[2]))
+    
+    
+    x = x.strip().split("-")
+    try:
+        date_of_birth = date(year=int(x[0]),month=int(x[1]),day=int(x[2]))
+    except ValueError:
+        return "Invalid date"
+    # today = date.today()
     days = (today - date_of_birth).days
     minutes = days * 24 * 60
-    
-    print(minutes / 24 / 60 / 365)
-    
+    return p.number_to_words(minutes, andword="").capitalize() + " minutes"
+
+
 if __name__ == "__main__":
     main()
-    
+
---
+++ CS50/Python/Week 8/test_seasons.py
@@ -0,0 +1,11 @@
+from seasons import process
+import pytest
+
+def test_valid_date():
+    assert process("1988-01-31", "2026-06-17") == "Twenty million, one hundred and eighty-four thousand, four hundred and eighty minutes"
+    assert process("2026-06-16", "2026-06-17") == "One thousand, four hundred and forty minutes"
+    assert process("2001-01-01", "2003-01-01") == "One million, fifty-one thousand, two hundred minutes minutes"
+
+def test_invalid_date():
+    with pytest.raises(ValueError):
+        process("2026-06-32", "2026-06-17")
