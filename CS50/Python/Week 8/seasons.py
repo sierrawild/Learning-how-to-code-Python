670adadb5a917d68cb09888eeff8d0132c6d6512
Pawel <pawelbabiak@gmail.com>
2026-06-18 21:58:24 +0100

    still working on the problem

--- CS50/Python/Week 8/seasons.py
+++ CS50/Python/Week 8/seasons.py
@@ -1,25 +1,34 @@
 from datetime import date
-import inflect
+import inflect, sys
 p = inflect.engine()
-today = date.today()
-today = "2003-01-01"
+# today = date.today()
+# today = "2003-01-01"
 
 def main():
-    # dob = input("When wore you born (format: YYYY-MM-DD): ")
-    dob = "2001-01-01"
-    print(process(dob, today))
+    dob = input("When wore you born (format: YYYY-MM-DD): ")
+    # dob = "2001-01-01"
+    
+    try:
+        print(process(dob, today))
+    except ValueError:
+        sys.exit("Invalid date")
 
-def process(x, today):
+def process(x, today=None):
+    if today is None:
+        today = date.today()
+    
     if type(today) == str:
         today = today.strip().split("-")
-        today = date(year=int(today[0]),month=int(today[1]),day=int(today[2]))
-    
-    
+        try:
+            today = date(year=int(today[0]),month=int(today[1]),day=int(today[2]))
+        except ValueError:
+            raise ValueError('Invalid date')
+
     x = x.strip().split("-")
     try:
         date_of_birth = date(year=int(x[0]),month=int(x[1]),day=int(x[2]))
     except ValueError:
-        return "Invalid date"
+        raise ValueError('Invalid date')
     # today = date.today()
     days = (today - date_of_birth).days
     minutes = days * 24 * 60
