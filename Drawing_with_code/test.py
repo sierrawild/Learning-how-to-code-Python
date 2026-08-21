5e3827bbe57dc2a4156b09b08af95ea0a9c7bd6b
Pawel <pawelbabiak@gmail.com>
2026-08-21 21:45:13 +0100

    Play

--- Drawing_with_code/test.py
+++ Drawing_with_code/test.py
@@ -1,5 +1,18 @@
-import random
+import turtle
 
-offset = random.sample(range(-15, 15), 5)
+t = turtle.Turtle()
+t.speed(0)
 
-print(offset)
+this_thing = 0
+
+while this_thing < 400:
+    
+    t.fd(100 + this_thing)
+    t.right(89)
+    this_thing = this_thing + 1
+    print(this_thing)
+    
+    
+
+
+turtle.done()
