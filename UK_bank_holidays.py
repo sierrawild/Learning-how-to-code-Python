2526042a35874af12f29d4e9df233cb1f15f672e
Pawel <pawelbabiak@gmail.com>
2026-09-01 22:40:24 +0100

    Update division and year

--- UK_bank_holidays.py
+++ UK_bank_holidays.py
@@ -4,10 +4,8 @@ CHOSE_YEAR = 2026
 
 bank_holidays = BankHolidays()
 bank_holidays = BankHolidays(locale='en')
-for bank_holiday in bank_holidays.get_holidays():
-    year = bank_holiday['date'].year
-    if year == CHOSE_YEAR:
-        print(bank_holiday['title'], 'is on', bank_holiday['date'])
+for bank_holiday in bank_holidays.get_holidays(division='england-and-wales', year=CHOSE_YEAR):
+    print(bank_holiday['title'], 'is on', bank_holiday['date'])
 print(bank_holidays.get_next_holiday())
 
 # see govuk_bank_holidays/bank_holidays.py source file for more methods and argument details…
