06d31765889f60fae7e4804479cf9707788d6f50
Pawel <pawelbabiak@gmail.com>
2026-09-15 10:12:49 +0100

    pickup setup

--- PyGame/Star Hunter/main.py
+++ PyGame/Star Hunter/main.py
@@ -1,8 +1,10 @@
-import pygame
+import pygame, random
 
 WIDTH, HEIGHT = 1280, 720
 palette = {'bg': "#75E7F2",
-           '1': "#FF8484"}
+           'player': "#FF8484",
+           'star': "#FFF835",
+           }
 running = True
 
 
@@ -11,9 +13,14 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 dt = 0
 
+# player data
 player_pos = pygame.Vector2(WIDTH/2,HEIGHT/2)
 player_speed = 300
 player_size = 25
+
+# enemies and pickups
+star_pos = pygame.Vector2(random.randint(0,WIDTH), random.randint(0,HEIGHT))
+
 
 def key_input(dt, player_pos, player_speed):
     keys = pygame.key.get_pressed()
@@ -39,13 +46,15 @@ while running:
             
     screen.fill(palette['bg'])
     ### Update ###
+    # player related update
     key_input(dt, player_pos, player_speed)
-    
     edge_wrap(WIDTH, HEIGHT, player_pos)
 
+    # star related update
     
     ### Draw ###
-    pygame.draw.rect(screen, palette['1'], (player_pos.x, player_pos.y, player_size, player_size)) # player
+    pygame.draw.rect(screen, palette['star'], (int(star_pos.x), int(star_pos.y), int(player_size * 0.7), int(player_size * 0.7))) # star
+    pygame.draw.rect(screen, palette['player'], (player_pos.x, player_pos.y, player_size, player_size)) # player
 
     
     pygame.display.flip()
